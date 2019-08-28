ff7c8ea67d90fd45245a2634d481abd1983ae730
keiing <49868852+keiing@users.noreply.github.com>
2019-08-29 00:19:57 +0800

    Promise

--- 5-1.promise/5-5.promise.js
+++ 5-1.promise/5-5.promise.js
@@ -1,19 +1,60 @@
 const Promise=require("./promise");
 
 //如果一个Promise then的方法中返回了一个普通值
-
+/**
 let p=new Promise((resolve,reject)=>{
-    resolve("路漫漫")
-});
+    resolve(new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            resolve('ppp')
+            // resolve("路漫漫")
+        })
+    }))
+
+    });
 let p2=p.then(data=>{
+    throw new Error('err')
     console.log(data+"吾应而上下求其知知，而知所以然");
     return data+"吾应而上下求其知知，而知所以然";//意思就是 我等待着我写作业 卡死了
 })
-p2.then(data=>{
-    console.log(data,'data2---');
-    return data;
-},(err)=>{
-    console.log(err)
-}).then(data=>{
-    console.log(data,"持之以恒方程也")
+p2.then(null,(err)=>{
+    console.log(err,'111111')
+}).then(null,err=>{
+    console.log(err,"持之以恒方程也")
 })
+ */
+let p=new Promise((resolve,reject)=>{
+    resolve(new Promise((resolve,reject)=>{
+
+        setTimeout(()=>{
+            reject('路漫漫')
+            // resolve("路漫漫")
+        },1000)
+    }))
+
+    }).then(data=>{
+    console.log(data+"吾应而上下求其知知，而知所以然");
+    // return data+"吾应而上下求其知知，而知所以然";//意思就是 我等待着我写作业 卡死了
+}).catch(err=>{
+    console.log(err,"err")
+})
+
+//Promise.finally 最终的 无论如何都执行
+//Promise.try()
+
+// let p2=new Promise((resolve,reject)=>{
+//     resolve();
+// })
+//finally这里出错了
+Promise.resolve(123).then(data=>{console.log(data)}).finally(()=>{
+    console.log('hellow')
+})
+// Promise.resolve(123).finally(()=>{
+//     console.log('finally')
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{resolve()},3000)
+//         console.log('hello')
+//     })
+// }).then(data=>{
+//     console.log(data,'err')
+// })
--- 5-1.promise/promise.js
+++ 5-1.promise/promise.js
@@ -8,18 +8,18 @@ const resolvePromise = (promise2, x, resolve, reject) => {
         return reject(new TypeError('Chaining cycle detected for promise #<Promise>'))
     }
     // 判断x 是不是一个普通值 先认为你是一个promise
-    if ((typeof x === 'object' && x != null) || typeof x === 'function') {
+    if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
+        let called;//默认没有调用成功 和失败
         //可能是promise 判断是不是promise then
-        try {
+        try {//取then的时候 可能会发生异常 getter
             let then = x.then; //看一看有没有then方法
-            let called;//默认没有调用成功 和失败
-            if(typeof then ==='function'){ //{then:function(){}}
-                //时promise了
-                then.call(x,(y)=>{//如果时一个promise 就用这个promise的结果
+            if(typeof then === 'function'){ //{then:function(){}}
+                //时promise了 让函数调用即可
+                then.call(x,y=>{//如果时一个promise 就用这个promise的结果 
                     if(called)return;
-                    called=true;
-                    resolvePromise(y)
-                },(r)=>{
+                    called=true; //y有可能还是个promise
+                    resolvePromise(promise2,y,resolve,reject)
+                },r=>{
                     if(called)return;//防止多次调用
                     called=true;
                     reject(r)
@@ -28,7 +28,7 @@ const resolvePromise = (promise2, x, resolve, reject) => {
                 resolve(x);//常量直接抛出去就行了
             }
         } catch (err) {
-            if(called)return;//防止多次调用
+            if(called) return;//防止多次调用
             called=true;
             reject(err)
         }
@@ -40,15 +40,20 @@ const resolvePromise = (promise2, x, resolve, reject) => {
 class Promise {
     constructor(executor) {
         //创建promise executor会立即执行
-        this.value = undefined;
-        this.reason = undefined;
+        this.value = undefined;//成功的值
+        this.reason = undefined;//失败的之
         this.status = PENDING;
         this.onResolvedCallback = [];
         this.onRejectedCallback = [];
         this.onRejected;
-        let resolve = (value) => {
-            if (this.status === PENDING) {
-                this.value = value;
+        let resolve = value => {//成功
+            //如果一个promsie resolve中传过来的是一个新的promise()就会等到这个内部的promise执行完成
+            if(value instanceof Promise){
+                //就让 vlaue.then(resolve,reject)执行 返回结果 再去调用成功或者失败
+                return value.then(resolve,reject);
+            }
+            if (this.status === PENDING) {//只有当状态时pending的时候
+                this.value = value;//将值保存起来
                 this.status = FULFILLED;
                 this.onResolvedCallback.forEach(fn => fn()); //发布有可能resolve在then的后面执行，此时先将方法存放起来，到时候依次执行
             }
@@ -61,14 +66,14 @@ class Promise {
             }
         };
         //这里可能会发生异常
-        try {
-            executor(resolve, reject);
+        try {//如果出错
+            executor(resolve, reject);//出错了 直接退出即可
         } catch (e) { //发生错误
             reject(e)
         }
     }
     //then 方法会判断当前的状态
-    then(onFulfilled, onRejected) {
+    then(onFulfilled, onRejected) {//两个回调时可选参数
         //默认参数
         onFulfilled=typeof onFulfilled === 'function'?onFulfilled:val=>val;
         onRejected=typeof onRejected === 'function'?onRejected:err=>{throw err};
@@ -115,13 +120,38 @@ class Promise {
                         } catch (err) {
                             reject(err)
                         }
-                    })
-                })
+                    });
+                });
             }
         })
         return promise2
     }
+    catch(errCallback){//没有成功的then
+        return this.then(null,errCallback)
+    }
+    static reject(reason){//创建一个失败的promise
+     return new Promise((resolve,reject)=>{
+         reject(reason);
+     })   
+    }
+    static resolve(value){//创建一个成功的promise
+        return new Promise((resolve,reject)=>{
+            resolve(value);
+        })
+    }
 }
+
+/** promises-aplus-tests promise.js*/
+Promise.deferred=function(){
+    let dfd={};
+    dfd.promise=new Promise((resolve,reject)=>{
+        dfd.resolve=resolve;
+        dfd.reject=reject;
+    })
+    return dfd;
+}
+ 
+
 //导出当前类 commonjs定义方式 export default 是ES6写法node不支持
 module.exports = Promise
 
