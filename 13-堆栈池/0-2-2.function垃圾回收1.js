81cf51567332d5cd288c578193ad9606675923d7
keiing <49868852+keiing@users.noreply.github.com>
2019-09-10 09:22:31 +0800

    Update 0-2-2.function垃圾回收1.js

--- 13-堆栈池/0-2-2.function垃圾回收1.js
+++ 13-堆栈池/0-2-2.function垃圾回收1.js
@@ -1,25 +1,27 @@
-function findGf() {
-    this.name = "mm";
-    this.lag = 160;
+function fn() {
+    this.name = "fn";
+    this.age= 18;
 }
-findGf.prototype.selectName = function () {
+fn.prototype.selectName = function () {
     let name = this.name;//闭包 js垃圾回收机制不回去回收
-    return function () {
+    return function () { //return 不影响外层函数执行
         return name;
     }//垃圾回收机制没有回收，闭包常驻内存
-    name = null;/**释放闭包 但是return 之后name=null还会再执行吗 
+    // name = null;
+    /**释放闭包 但是return 之后name=null还会再执行吗 
     通过打印process.memoryUsage()
     发现
     加上 name = null，heapUsed: 4236288,
     不加 heapUsed: 4236296,由此得出是有效果的
     */
 }
-let gf = new findGf();
+let gf = new fn();
+console.log(gf.selectName()())
 
-// console.log(gf.selectName()())
+console.log(process.memoryUsage());//测试
 /**
  * { rss: 21925888,
-  heapTotal: 6537216,
+  heapTotal: 6537216,3
   heapUsed: 3986848,
   external: 8272 }
  */
@@ -46,3 +48,35 @@ let o={
  o2='willson'//"willson"
 oa=null;//释放内存
 console.log(process.memoryUsage());//测试
+
+
+
+
+
+
+
+
+
+
+function fn() {
+    this.name = "fn";
+    this.age= 18;
+}
+let name="zhang"
+fn.prototype.selectName = function () {
+    let name = this.name;
+    return function () { //return 不影响外层函数执行
+        return name;
+    }
+    name = null;//所以name=null这一步也是 执行的
+}
+let gf = new fn();
+console.log(gf.selectName()())
+console.log(name)
+console.log(process.memoryUsage());//
+
+/**
+ * rss：驻留集大小
+ * heapTotal：堆的总大小
+ * heapUsed：实际使用的堆
+ */
